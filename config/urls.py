573fe534f0d7c6e46b1e76fb2966b7a193118682
Sthompson <shelby.thompson@mga.edu>
2025-04-28 21:46:07 -0400

    Updates Rest html

--- config/urls.py
+++ config/urls.py
@@ -1,31 +1,13 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-# config/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),  # Admin URL
     path('', include('myapp.urls')),  # This includes URLs for myapp
     path('restaurant/', include('myapp.restaurant.urls')),  # This includes URLs for the restaurant app
-    path('calculator/', include('calculator.urls')),  # This connects your app’s URLs
-    path('bmi/', include('BMI_Calculator.urls')),  # Added BMI_Calculator app URLs
-    # path('calorie/', include('myapp.EL_caltrack.urls')),  # Uncomment to include calorie tracker URLs
+    path('calculator/', include('calculator.urls')),  # Connects your calculator app's URLs
+    path('bmi/', include('BMI_Calculator.urls')),  # Includes URLs for BMI_Calculator app
+    # Removed the line for the calorie tracker app
 ]
 
 
